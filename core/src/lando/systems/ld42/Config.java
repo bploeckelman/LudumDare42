42b78386c83f1a7d327c5e86ec708c44cc7e77b7
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2018-08-11 20:54:21 -0500

    Change peasant sprite and player color

--- core/src/lando/systems/ld42/Config.java
+++ core/src/lando/systems/ld42/Config.java
@@ -8,6 +8,6 @@ public class Config {
     public static final int window_height = 600;
     public static final boolean resizable = false;
     public static final Color background_color = new Color(0f, .25f, .7f, 1f);
-    public static final Color player_color = new Color(255f / 255f, 242f / 255f, 0f, 1f);
+    public static final Color player_color = new Color(0f / 255f, 183f / 255f, 239f / 255f, 1f);
     public static final Color enemy_color = new Color(227f / 255f, 28f / 255f, 36f / 255f, 1f);
 }
