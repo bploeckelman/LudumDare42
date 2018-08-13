eb2d59f073b79da21538e665d462ece2200f2520
Doug Graham <doug.graham@wisc.edu>
2018-08-13 15:56:06 -0500

    Fix up tutorial

--- core/src/lando/systems/ld42/utils/HelpModalWindow.java
+++ core/src/lando/systems/ld42/utils/HelpModalWindow.java
@@ -22,9 +22,9 @@ public class HelpModalWindow extends ModalWindow {
             " -  Soldiers are all-rounder unit",
             " Action Phase",
             " - Move a unit to an empty tile or move onto an enemy unit to attack",
-            " - You can swap unit's tile by moving onto friendly unit",
+            " - You can swap units by moving onto a tile with a friendly unit",
             " Victory Condition",
-            " - Capture enemy castle or hold all tiles except enemy castle"
+            " - Capture the enemy castle or hold all tiles except the enemy castle"
     };
 
     public HelpModalWindow(OrthographicCamera camera) {
@@ -44,14 +44,13 @@ public class HelpModalWindow extends ModalWindow {
             final float target_width = modalRect.width;
             LudumDare42.game.assets.font.getData().setScale(title_text_scale);
             LudumDare42.game.assets.fontShader.setUniformf("u_scale", title_text_scale);
-            LudumDare42.game.assets.layout.setText(LudumDare42.game.assets.font, "Kingdoms Fall Tutorial",
+            LudumDare42.game.assets.layout.setText(LudumDare42.game.assets.font, "Tutorial",
                     Color.WHITE, target_width, Align.center, true);
             LudumDare42.game.assets.font.draw(batch, LudumDare42.game.assets.layout,
                     modalRect.x + margin_left,
                     modalRect.y + modalRect.height - margin_top);
             LudumDare42.game.assets.font.setColor(Color.WHITE);
             LudumDare42.game.assets.font.getData().setScale(1f);
-            LudumDare42.game.assets.fontShader.setUniformf("u_scale", 1f);
         }
 
         for (int i=0;i<introTutorial.length;i++) {
@@ -68,10 +67,10 @@ public class HelpModalWindow extends ModalWindow {
                         modalRect.y + modalRect.height - margin_top * MARGIN_MULTIPLIER * (i + 2));
                 LudumDare42.game.assets.font.setColor(Color.WHITE);
                 LudumDare42.game.assets.font.getData().setScale(1f);
-                LudumDare42.game.assets.fontShader.setUniformf("u_scale", 1f);
             }
         }
 
+        float unitOffeset = 150;
         for (int i=0;i<tutorial.length;i++) {
             batch.setShader(LudumDare42.game.assets.fontShader);
             {
@@ -81,36 +80,38 @@ public class HelpModalWindow extends ModalWindow {
                 LudumDare42.game.assets.fontShader.setUniformf("u_scale", title_text_scale);
                 LudumDare42.game.assets.layout.setText(LudumDare42.game.assets.font, tutorial[i],
                         Color.WHITE, target_width, Align.left, true);
+                //Hard code all the things
+                float xOffset = 0;
+                if (i > 2 && i < 7) xOffset = unitOffeset;
                 LudumDare42.game.assets.font.draw(batch, LudumDare42.game.assets.layout,
-                        modalRect.x + margin_left,
+                        modalRect.x + margin_left + xOffset,
                         modalRect.y + modalRect.height - margin_top * MARGIN_MULTIPLIER * (i+introTutorial.length+2));
                 LudumDare42.game.assets.font.setColor(Color.WHITE);
                 LudumDare42.game.assets.font.getData().setScale(1f);
-                LudumDare42.game.assets.fontShader.setUniformf("u_scale", 1f);
             }
         }
         batch.setShader(null);
 
         batch.draw(LudumDare42.game.assets.unitAnimationPeasant.getKeyFrame(0),
-                modalRect.x + margin_left * 1.5f,
+                modalRect.x + margin_left * 1.5f + unitOffeset,
                 modalRect.y + modalRect.height - margin_top * 2f - margin_top * MARGIN_MULTIPLIER * (introTutorial.length+5),
                 LudumDare42.game.assets.unitAnimationPeasant.getKeyFrame(0).getRegionWidth() * 1.5f,
                 LudumDare42.game.assets.unitAnimationPeasant.getKeyFrame(0).getRegionHeight() * 1.5f);
 
         batch.draw(LudumDare42.game.assets.unitAnimationArcher.getKeyFrame(0),
-                modalRect.x + margin_left * 1.5f,
+                modalRect.x + margin_left * 1.5f + unitOffeset,
                 modalRect.y + modalRect.height - margin_top * 2f - margin_top * MARGIN_MULTIPLIER * (introTutorial.length+6),
                 LudumDare42.game.assets.unitAnimationArcher.getKeyFrame(0).getRegionWidth() * 1.5f,
                 LudumDare42.game.assets.unitAnimationArcher.getKeyFrame(0).getRegionHeight() * 1.5f);
 
         batch.draw(LudumDare42.game.assets.unitAnimationWizard.getKeyFrame(0),
-                modalRect.x + margin_left * 1.5f,
+                modalRect.x + margin_left * 1.5f + unitOffeset,
                 modalRect.y + modalRect.height - margin_top * 2f - margin_top * MARGIN_MULTIPLIER * (introTutorial.length+7),
                 LudumDare42.game.assets.unitAnimationWizard.getKeyFrame(0).getRegionWidth() * 1.5f,
                 LudumDare42.game.assets.unitAnimationWizard.getKeyFrame(0).getRegionHeight() * 1.5f);
 
         batch.draw(LudumDare42.game.assets.unitAnimationSoldier.getKeyFrame(0),
-                modalRect.x + margin_left * 1.5f,
+                modalRect.x + margin_left * 1.5f + unitOffeset,
                 modalRect.y + modalRect.height - margin_top * 2f - margin_top * MARGIN_MULTIPLIER * (introTutorial.length+8),
                 LudumDare42.game.assets.unitAnimationSoldier.getKeyFrame(0).getRegionWidth() * 1.5f,
                 LudumDare42.game.assets.unitAnimationSoldier.getKeyFrame(0).getRegionHeight() * 1.5f);
